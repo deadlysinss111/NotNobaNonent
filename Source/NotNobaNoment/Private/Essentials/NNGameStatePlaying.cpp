5c3c00b47ad1998c008d1a753a7668452315df03
Baptiste-GC-Tech <117664376+Baptiste-GC-Tech@users.noreply.github.com>
2025-02-04 11:03:41 +0100

    GameState skeleton

--- Source/NotNobaNoment/Private/Essentials/NNGameStatePlaying.cpp
+++ Source/NotNobaNoment/Private/Essentials/NNGameStatePlaying.cpp
@@ -3,3 +3,26 @@
 
 #include "Essentials/NNGameStatePlaying.h"
 
+DEFINE_LOG_CATEGORY(NN_GameStatePlaying);
+
+	/*  -----------    */
+   /*  CLASS METHODS  */
+  /*    -----------  */
+
+// Constru & Destru
+ANNGameStatePlaying::ANNGameStatePlaying()
+{
+	UWorld* world = GetWorld();
+
+	if (world != nullptr)
+	{
+		AuthorityGameMode = world->GetAuthGameMode();		// <-- Should always be correct since GameStates are instantiated by a GameMode. This line may be redundant, I'm just making sure.
+		GameModeClass = AuthorityGameMode ? AuthorityGameMode->GetClass() : nullptr;
+	}
+
+	UE_LOG(NN_GameStatePlaying, Log, TEXT("Instantiated GameStatePlaying ! :D"));
+}
+ANNGameStatePlaying::~ANNGameStatePlaying()
+{
+	// Nothing yet
+}
--- Source/NotNobaNoment/Public/Essentials/NNGameStatePlaying.h
+++ Source/NotNobaNoment/Public/Essentials/NNGameStatePlaying.h
@@ -4,14 +4,33 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameStateBase.h"
+
+// Extra includes
+
 #include "NNGameStatePlaying.generated.h"
 
 /**
+ *  Custom Game State (Currently Playing through a level)
  * 
+ *  This is meant to hold & manage :
+ *   - Level state
+ *   - Level accesses
+ *   - Level difficulty
+ *   - ...
  */
+
+DECLARE_LOG_CATEGORY_EXTERN(NN_GameStatePlaying, Log, All);
+
 UCLASS()
 class NOTNOBANOMENT_API ANNGameStatePlaying : public AGameStateBase
 {
 	GENERATED_BODY()
-	
+
+	/*  -----------    */
+   /*  CLASS METHODS  */
+  /*    -----------  */
+public:
+	// Constru & Destru
+	ANNGameStatePlaying();
+	~ANNGameStatePlaying();
 };
